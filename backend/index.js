eee4d7f2ff8e3f3eab62d40302223febaa382ad8
riteshh82 <yadavritesh6655@gmail.com>
2024-04-18 16:56:49 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import contact from "./Routes/contact.js";
 import "./Connection/conn.js";
-import dotenv from 'dotenv';
-const { config } = dotenv;
-config();
+// import dotenv from 'dotenv';
+// const { config } = dotenv;
+// config();
 
 const server = express();
 
@@ -12,12 +12,12 @@ server.use(express.json());
 server.use(cors());
 server.use('/api/v1', contact);
 
-const port = process.env.REACT_APP_PORT;
+// const port = process.env.REACT_APP_PORT;
 
 server.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
-server.listen(port, () => {
-  console.log(`Server started at ${port}`);
+server.listen(2000, () => {
+  console.log(`Server started at 2000`);
 });
